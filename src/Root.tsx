79deb15d8ab389755cb0c15d631dc5d2a3a2b4c7
silcondiox1de <yujiaguo1227@gmail.com>
2025-11-28 01:23:59 -0800

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage";
-import ToolApp from "./ToolApp"; // your tab editor
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
+import Tool from './ToolApp'; 
 
 export default function Root() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Route 1: The Landing Page (Home) */}
         <Route path="/" element={<LandingPage />} />
-        <Route path="/tab" element={<ToolApp />} />
+
+        {/* Route 2: The Tool */}
+        <Route path="/tool/tab" element={<Tool />} />
       </Routes>
     </BrowserRouter>
   );
