65fd57ae59af9da75cde9d84b1dfaa1c24b30e9a
silcondiox1de <yujiaguo1227@gmail.com>
2025-11-28 00:08:24 -0800

    update1

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Standard Vite + React app config
 export default defineConfig({
   plugins: [react()],
+  root: ".",
   build: {
     outDir: "dist",
   },
